66496f8a01cffd7256847edf0e72625f87676b41
Oluwamaintain123 <137342690+Oluwamaintain123@users.noreply.github.com>
2025-04-13 02:03:13 -1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function showSidebar(){
+
+    const sidebar = document.querySelector('.navMenu');
+    sidebar.style.display = 'flex';
+
+}
+
+function hideSidebar(){
+
+    const sidebar = document.querySelector('.navMenu');
+    sidebar.style.display = 'none';
+    
+}
+
+// function like(){
+
+//     const more = document.querySelector('.hart');
+//     more.style.color = 'red';
+
+// }
